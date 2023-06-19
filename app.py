9df65a738ccae6646c75e7a20cfb8d6b822753a2
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-19 21:58:37 +0300

    added player & converted all images

--- app.py
+++ app.py
@@ -8,18 +8,21 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
-ground_surface = pygame.image.load('platformer_resources/ground.png')
+ground_surface = pygame.image.load('platformer_resources/ground.png').convert()
 ground_surface = pygame.transform.scale(ground_surface, (800,120))
 
 sky_surface = pygame.Surface((800,500))
 sky_surface.fill('cyan3')
 
-clouds_surface = pygame.image.load("platformer_resources/clouds.png")
+clouds_surface = pygame.image.load("platformer_resources/clouds.png").convert()
 clouds_surface = pygame.transform.scale(clouds_surface, (550,150))
-
 
 text_font = pygame.font.Font(None, 30)
 text_surface = text_font.render("Once Upon A Time ...", False, "darkblue")
+
+player_surface = pygame.image.load("platformer_resources/player1.png").convert()
+player_surface = pygame.transform.scale_by(player_surface, 3.5)
+player_rect = player_surface.get_rect(midbottom = (60, 480))
 
 clouds_pos = 200
 
@@ -37,6 +40,7 @@ while True:
     screen.blit(ground_surface, (0,480))
     screen.blit(text_surface, (20, 20))
     screen.blit(clouds_surface, (clouds_pos, 60))
+    screen.blit(player_surface, player_rect)
     
     pygame.display.update()
     clock.tick(60)
