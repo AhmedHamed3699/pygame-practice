40634742ab477d6b512f654885f12b92a02a38d0
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-19 19:56:58 +0300

    Added Text

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Game")
 clock = pygame.time.Clock()
 
-ground_surface = pygame.image.load('Platformer_2/ground.png')
+ground_surface = pygame.image.load('platformer_resources/ground.png')
 ground_surface = pygame.transform.scale(ground_surface, (800,120))
 
 sky_surface = pygame.Surface((800,500))
 sky_surface.fill('aquamarine3')
+
+text_font = pygame.font.Font(None, 50)
+text_surface = text_font.render("Let's Go!", False, "coral")
 
 while True:
     for event in pygame.event.get():
@@ -22,6 +25,7 @@ while True:
     
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,480))
+    screen.blit(text_surface, (330, 100))
     
     pygame.display.update()
     clock.tick(60)
