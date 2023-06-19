59c2b470826c717589d5c7b63cb25ad9f236cd89
Ahmed Hamed <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-06-19 21:32:12 +0300

    added some clouds animation

--- app.py
+++ app.py
@@ -12,20 +12,31 @@ ground_surface = pygame.image.load('platformer_resources/ground.png')
 ground_surface = pygame.transform.scale(ground_surface, (800,120))
 
 sky_surface = pygame.Surface((800,500))
-sky_surface.fill('aquamarine3')
+sky_surface.fill('cyan3')
 
-text_font = pygame.font.Font(None, 50)
-text_surface = text_font.render("Let's Go!", False, "coral")
+clouds_surface = pygame.image.load("platformer_resources/clouds.png")
+clouds_surface = pygame.transform.scale(clouds_surface, (550,150))
+
+
+text_font = pygame.font.Font(None, 30)
+text_surface = text_font.render("Once Upon A Time ...", False, "darkblue")
+
+clouds_pos = 200
 
 while True:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             pygame.quit()
             exit()
-    
+            
+    if clouds_pos == -580:
+        clouds_pos = 800
+
+    clouds_pos -= 2
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,480))
-    screen.blit(text_surface, (330, 100))
+    screen.blit(text_surface, (20, 20))
+    screen.blit(clouds_surface, (clouds_pos, 60))
     
     pygame.display.update()
     clock.tick(60)
